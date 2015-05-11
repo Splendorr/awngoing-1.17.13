1cc8ef9affbb27ba0c5f250d9083ef0358248c9b
Splendorr <simolinic@gmail.com>
2015-05-11 00:26:47 -0400

    Messy fix for ordering most-recent by hacky :update_at: timestamps.

--- soups/system/most-recent.snip.rb
+++ soups/system/most-recent.snip.rb
@@ -1,6 +1,7 @@
 class MostRecent < Dynasnip
   def handle(arguments)
-    snips = app.soup[:kind => arguments[:kind]].sort_by { |s| s.created_at || -1 }.reverse
+    # snips = app.soup[:kind => arguments[:kind]].sort_by { |s| s.created_at || -1 }.reverse
+    snips = app.soup[:kind => arguments[:kind]].sort_by { |s| s.updated_at.to_s || -1 }.reverse
     offset = (arguments[:offset] || 0).to_i
     limit = offset + (arguments[:count].to_i-1)
     snips[offset..limit].map { |s| "{in-template snip:#{s.name}, template:#{arguments[:template]}}" }.join("")
